0bc24f466a5bb493e50af5412b30d4632bed0bcd
Rostyslav <rostyslav@Rostyslavs-MacBook-Pro.local>
2018-01-27 10:58:10 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TableViewComponent } from './table-view/table-view.component';
+import {TableViewService} from "./table-view/table-view.service";
+import {HttpClientModule} from "@angular/common/http";
+import { LoginComponent } from './login/login.component';
+import {FormsModule} from "@angular/forms";
+import {AuthService} from "./login/auth.service";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableViewComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [TableViewService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
